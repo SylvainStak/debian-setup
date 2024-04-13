a006bf88afbbf63bd153869fcf380bcfc2ef10ad
SylvainStak <stackersap@gmail.com>
2024-04-13 18:46:11 +0100

    test

--- setup.sh
+++ setup.sh
@@ -45,6 +45,6 @@ sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
 sudo cat <<EOF > /etc/systemd/system/getty@tty1.service.d/override.conf
 [Service]
 ExecStart=
-ExecStart=-/sbin/agetty --autologin root --noclear %I 38400 linux
+ExecStart=-/sbin/agetty --autologin stacker --noclear %I 38400 linux
 EOF
 systemctl enable getty@tty1.service
