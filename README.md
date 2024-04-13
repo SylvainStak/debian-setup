c5e8836db14804719464e244f36b4978f69bd82f
SylvainStak <stackersap@gmail.com>
2024-04-13 13:58:23 +0100

    test

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # debian-setup
+
+first do a minimal debian install, then:
+```
+su -
+apt install sudo
+echo "stacker ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/sudoer_stacker
+exit
+
+cd
+sudo apt install git
+git clone https://github.com/SylvainStak/debian-setup
+cd debian-setup
+chmod +x setup.sh
+sudo ./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Ensuring the script is run with root privileges
 if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root" 1>&2
+  echo "setup.sh must be run as root" 1>&2
   exit 1
 fi
 
-# Updating and upgrading the system
-echo "Updating package lists..."
-apt-get update
-apt-get upgrade -y
+# Update system
+apt update && apt upgrade -y
 
-# Installing i3 and alacritty
-echo "Installing i3 and alacritty..."
-apt-get install -y i3 alacritty
+# Install packages
+packages=(
+  i3
+  alacritty
+)
 
-echo "Installation complete!"
+for package in "${packages[@]}"; do
+  apt install -y "$package"
+done
